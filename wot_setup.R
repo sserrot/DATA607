c8e1c100b46feeb976563bf6b55e7ced6437b3d5
Santiago Torres <sserrot@users.noreply.github.com>
2021-12-03 23:27:16 -0500

    Abstract out book parsing logic and combine dataframes

--- wot_setup.R
+++ wot_setup.R
@@ -10,48 +10,60 @@ library(ggplot2)
 path_to_pdf <- here("GitHub","DATA606","Project","WoT")
 
 files <- list.files(path = path_to_pdf, pattern = "pdf$")
-test <- str_c(path_to_pdf, "/", files[1])
-wot <- lapply(test, pdf_text)
+
+# abstract out to now manually pull each dataframe.
+complete_path <- function(x) { str_c(path_to_pdf, "/",x)}
+files <- complete_path(files)
 wot <- lapply(files, pdf_text)
+# end 
 
-dragon_reborn <- wot[[1]]
-dragon_reborn_df <- tibble(book = "The Dragon Reborn", page = 1:length(dragon_reborn), text = dragon_reborn)
+
+
+cos_book <- wot[[1]]
+title <- cos_book[1] %>% str_split("\n")
+title <- title[[1]][1]
+cos_book_df <- tibble(book = title, page = 1:length(cos_book), text = cos_book)
+cos_book_df <- cos_book_df %>% unnest_tokens(word, text)
+
+
+dragon_reborn <- wot[[2]]
+title <- dragon_reborn[1] %>% str_split("\n")
+title <- title[[1]][1]
+dragon_reborn_df <- tibble(book = title, page = 1:length(dragon_reborn), text = dragon_reborn)
 dragon_reborn_df <- dragon_reborn_df %>% unnest_tokens(word, text)
-
-# remove stop words
-
 data(stop_words)
 
-dragon_reborn_df <- dragon_reborn_df %>% anti_join(stop_words)
+testing_function <- function(list) {
+  data(stop_words)
+  title <- list[1] %>% str_split("\n")
+  title <- title[[1]][1]
+  pdf_book <- tibble(book = title, page=1:length(list), text = list)
+  pdf_book <- pdf_book %>% unnest_tokens(word, text)
+  pdf_book <- pdf_book %>% anti_join(stop_words)
+}
 
+
+all_books <- lapply(wot, testing_function)
+  
+  
+all_books <- all_books %>% reduce(rbind) # bind all dataframes together
+
+
+# remove stop words
 afinn <- get_sentiments("afinn")
+all_books <- all_books %>% inner_join(afinn)
+word_count <- all_books %>% count(word, sort= TRUE)
+wordcloud(word_count$word,word_count$n,  max.words = 20) # die - dice? died
 
-word_count <- dragon_reborn_df %>% inner_join(afinn) %>% count(word, sort= TRUE)
-wordcloud(word_count$word,word_count$n,  max.words = 50) # die - dice? died
-
-dragon_reborn_words <- dragon_reborn_df %>% inner_join(afinn) %>% group_by(word) %>% summarize(value = mean(value))
-
-dragon_reborn_pages <- dragon_reborn_df %>% inner_join(afinn) %>% group_by(page) %>% summarize(value = sum(value))
-
-dragon_reborn_value <- dragon_reborn_df %>% inner_join(afinn) %>% group_by(book) %>% summarize(value = sum(value))
-
-dragon_reborn_mean_value <- dragon_reborn_df %>% inner_join(afinn) %>% group_by(book) %>% summarize(value = mean(value))
+dragon_reborn_words <- all_books %>% inner_join(afinn) %>% group_by(word) %>% summarize(value = mean(value))
+dragon_reborn_words <- all_books %>% inner_join(afinn) %>% group_by(book,word) %>% summarize(value = mean(value))
+dragon_reborn_pages <- all_books %>% inner_join(afinn) %>% group_by(page) %>% summarize(value = sum(value))
+dragon_reborn_value <- all_books %>% inner_join(afinn) %>% group_by(book) %>% summarize(value = sum(value))
+dragon_reborn_mean_value <- all_books %>% inner_join(afinn) %>% group_by(book) %>% summarize(value = mean(value))
 
 
 ggplot(dragon_reborn_pages, aes(x=page,y=value)) + geom_point()
 
-dr_box <- dragon_reborn_df %>% inner_join(afinn)
-
-ggplot(dr_box, aes(x=book,y=value)) + geom_boxplot()
 
 
-
-
-
-
-eotw <- wot[[2]]
-eotw_df <- tibble(book = "The Eye Of The World", page = 1:length(eotw), text = eotw)
-eotw_df <- eotw_df %>% unnest_tokens(word, text)
-eotw_df <- eotw_df %>% anti_join(stop_words)
-
-eotw_df %>% count(word, sort=TRUE)
+ggplot(all_books, aes(x=book,y=value)) + geom_boxplot()
